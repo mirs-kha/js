f399e21939fe4f4857f50494fa539d0ad235bc6f
mirs-kha <130989395+mirs-kha@users.noreply.github.com>
2023-05-23 20:22:38 +0200

    Update game.js

--- qui/views/assets/js/game.js
+++ qui/views/assets/js/game.js
@@ -3,7 +3,31 @@ const chapeau = document.querySelector('#chapeau')
 var afflife = document.querySelector('#life')
 var life = 3
 
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 var btns2 = ["oreilles", "cornes", "tentacules", "cheveux", "chapeau", "lunettes", "barbe", "ailes", "moustache", "troisyeux", "deuxyeux", "oeil", "rose", "jaune", "vert", "violet", "multi"]
 var btns = document.getElementsByClassName("btn2")
@@ -386,7 +410,6 @@ Characters.forEach(element => {
     })
 
 });
-
 
 
 
