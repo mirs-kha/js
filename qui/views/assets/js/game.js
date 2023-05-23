7e375ffaa9c332863e8a684e78862439e199a35f
mirs-kha <130989395+mirs-kha@users.noreply.github.com>
2023-05-23 20:49:11 +0200

    Update game.js

--- qui/views/assets/js/game.js
+++ qui/views/assets/js/game.js
@@ -25,8 +25,10 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-btns.onclick = function() {
-    modal.style.display = "none";
+btns.onclick = function(events) {
+    if (events.target == modal) {
+        modal.style.display = "none";
+      }
   }
 
 // When the user clicks anywhere outside of the modal, close it
