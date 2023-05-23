34ab4f312b9435d29fb8142b5d9d963eb5385bad
mirs-kha <130989395+mirs-kha@users.noreply.github.com>
2023-05-23 21:31:10 +0200

    Update game.js

--- qui/views/assets/js/game.js
+++ qui/views/assets/js/game.js
@@ -4,7 +4,7 @@ var afflife = document.querySelector('#life')
 var life = 3
 
 var btns2 = ["oreilles", "cornes", "tentacules", "cheveux", "chapeau", "lunettes", "barbe", "ailes", "moustache", "troisyeux", "deuxyeux", "oeil", "rose", "jaune", "vert", "violet", "multi"]
-var btns = document.getElementsByClassName("btn2")
+var btns = document.querySelector('.btn2')
 
 // Get the modal
 var modal = document.getElementById("myModal");
